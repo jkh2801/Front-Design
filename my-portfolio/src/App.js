a6caffa00873e713a8e2622eadc146a1124a7d2b
jkh2801 <jkh2801@naver.com>
2020-11-07 14:38:58 +0900

    React-Portfolio

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,3 +1,4 @@
+import React, {useState, useEffect} from "react";
 import './App.css';
 import { BrowserRouter as Router, Link, Route} from "react-router-dom";
 
@@ -9,9 +10,32 @@ import Project from './components/Project'
 import Contact from './components/Contact';
 
 function App() {
+  let pre = 0;
+  let next = 1;
+  const heights = []
+  useEffect(() => {
+    const section = document.querySelectorAll("section")
+    section.forEach(function(s) {
+      heights.push(s.offsetTop - 200)
+    })
+  })
+  window.addEventListener('scroll', function() {
+    const nav = document.querySelector('.nav')
+    nav.classList.toggle("sticky", window.scrollY > 0)
+    if(window.scrollY >= heights[next]) {
+      pre++
+      next++
+      console.log("??")
+    }
+    if(window.scrollY <= heights[pre]) {
+      pre--
+      next--
+      console.log("???")
+    }
+})
   return (
     <div className="page">
-      <header><Nav/></header>
+      <header><Nav pre = {pre}/></header>
       <section id="home"><Home/></section>
       <section id="about"><About/></section>
       <section id="skills"><Skill/></section>
